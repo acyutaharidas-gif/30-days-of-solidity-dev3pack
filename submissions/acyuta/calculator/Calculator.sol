62e4cdc61aee430b5491c80487281610ea6ddd36
acyutaharidas-gif <acyutaharidas@gmail.com>
2026-03-19 16:49:30 +0000

    calculator

--- submissions/acyuta/calculator/Calculator.sol
+++ submissions/acyuta/calculator/Calculator.sol
@@ -1,3 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {ScientificCalculator} from "./ScientificCalculator.sol";
+
+contract Calculator {
+    // can use 1. interface - treat as object
+    // 2. abi encode - low level call when no address? but fujnction signature
+    address public owner;
+    address public scientificCalculatorAddress;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
+    function setScientificCalculator(address _address) public onlyOwner {
+        scientificCalculatorAddress = _address;
+    }
+
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+
+    function subtract(uint256 a, uint256 b) public pure returns (uint256) {
+        return a - b;
+    }
+
+    function multiply(uint256 a, uint256 b) public pure returns (uint256) {
+        return a * b;
+    }
+
+    function divide(uint256 a, uint256 b) public pure returns (uint256) {
+        require(b != 0, "Cannot divide by zero");
+        return a / b;
+    }
+
+    // interface
+    function calculatePower(
+        uint256 base,
+        uint256 exponent
+    ) public view returns (uint256) {
+        ScientificCalculator scientificCalculator = ScientificCalculator(
+            scientificCalculatorAddress
+        );
+        return scientificCalculator.power(base, exponent);
+    }
+
+    // low-level
+    function calculateSquareRoot(uint256 number) public returns (uint256) {
+        (bool success, bytes memory returnData) = scientificCalculatorAddress
+            .call(abi.encodeWithSignature("squareRoot(uint256)", number));
+        require(success, "External call failed");
+        return abi.decode(returnData, (uint256));
+    }
+}
---
+++ submissions/acyuta/calculator/ScientificCalculator.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ScientificCalculator {
+    function power(uint _base, uint _exponent) public pure returns (uint) {
+        if (_exponent == 0) return 1;
+        return (_base ** _exponent);
+    }
+
+    function squareRoot(uint256 number) public pure returns (uint256) {
+        require(number >= 0, "Cannot calculate square root of negative number");
+        if (number == 0) return 0;
+        // newton's method 
+        uint256 result = number / 2;
+        for (uint256 i = 0; i < 10; i++) {
+            result = (result + number / result) / 2;
+        }
+        return result;
+    }
+}
